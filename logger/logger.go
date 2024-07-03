5c22a9df04c789048a08677152bfda19c08a22e5
Greg Muller <otto.muller.general@gmail.com>
2024-07-04 00:35:46 +0400

    colorized logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,37 +1,52 @@
 package logger
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"github.com/ottoMuller1/base/nullable"
 )
-
 
 // colors
 func makeTextColorized(message string, color string) string {
 	return color + message + "\033[0m"
 }
 
+// rgb type
+type RGB struct {
+	R int
+	G int
+	B int
+}
+
+// rgb to ansi value
+func rgbToAnsi(rgb RGB) string {
+
+	return fmt.Sprintf("\u001b[38;2;%d;%d;%dm", rgb.R, rgb.G, rgb.B)
+
+}
+
 
 
 
 // log types
-type PrefixHandler func(string) string
+var NullPrefixHandler = nullable.Null[func (string) string]()
 
+// logger type
 type logger struct {
 	color  string
-	prefixHandler PrefixHandler
+	prefixHandler func (string) string
 }
 
-func New(color string, prefixHandlerNullable nullable.Nullable[PrefixHandler]) logger {
-
-	defaultPrefixHandler := func(s string) string {
-		return s
-	}
+// new logger
+func New(rgb RGB, prefixHandlerNullable nullable.Nullable[func (string) string]) logger {
 
 	return logger {
-		color: color,
-		prefixHandler: prefixHandlerNullable.FromNullable(defaultPrefixHandler, true),
+		color: rgbToAnsi(rgb),
+		prefixHandler: prefixHandlerNullable.FromNullable(
+			func(s string) string {return s},
+			true,
+		),
 	}
 
 }
@@ -39,7 +54,7 @@ func New(color string, prefixHandlerNullable nullable.Nullable[PrefixHandler]) l
 
 
 
-
+// basic loger terms
 var Error = logger {
 	color: "\033[31m",
 	prefixHandler: func(message string) string {return "ERROR: " + message + " "},
@@ -68,7 +83,7 @@ var Custom = logger {
 
 
 
-// logger fuction
+// log function
 func (lg logger) Log(message string, prefix string) {
 	logger := log.New(os.Stdout, makeTextColorized(lg.prefixHandler(prefix), lg.color), log.Ldate|log.Ltime)
 	logger.Printf(makeTextColorized(message, lg.color))

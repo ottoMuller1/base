8a937afd94c8b92b23a728f2a1eb0361b859793a
Greg Muller <otto.muller.general@gmail.com>
2024-10-07 20:03:14 +0400

    nullable json encode decode

--- nullable/nullable.go
+++ nullable/nullable.go
@@ -1,30 +1,62 @@
 package nullable
 
+import (
+	"encoding/json"
+)
 
 // nullable type
-type Nullable[t any] struct{
+type Nullable[t any] struct {
 	filled bool
 	Error  error
 	value  t
 }
 
+func (nl Nullable[T]) ToJSONByte() ([]byte, error) {
+	if !nl.filled {
+		return json.Marshal(nil)
+	}
+
+	return json.Marshal(
+		struct {
+			Value T `json:"value"`
+		}{
+			Value: nl.value,
+		},
+	)
+}
+
+func FromJSONByte[T any](data []byte) (*Nullable[T], error) {
+	var temp *struct {
+		Value T `json:"value"`
+	}
+
+	err := json.Unmarshal(data, &temp)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if temp == nil {
+		return nil, nil
+	}
+
+	return &Nullable[T]{true, nil, temp.Value}, nil
+}
 
 // null term
 func Null[t any]() Nullable[t] {
 	return Nullable[t]{filled: false}
 }
 
-
 // value to nullable
 func ToNullable[t any](value t) Nullable[t] {
 
-	return Nullable[t] {
+	return Nullable[t]{
 		filled: true,
-		value: value,
+		value:  value,
 	}
 
 }
-
 
 // value to nullable by pointer
 func ToNullablePointer[t any](value *t) Nullable[t] {
@@ -35,12 +67,10 @@ func ToNullablePointer[t any](value *t) Nullable[t] {
 
 	return Nullable[t]{
 		filled: true,
-		value: *value,
+		value:  *value,
 	}
 
 }
-
-
 
 // to pointer
 func (nullable Nullable[t]) ToPointer() *t {
@@ -53,23 +83,16 @@ func (nullable Nullable[t]) ToPointer() *t {
 
 }
 
-
-
-
 // pass an error use instead of .Err if inlinable Err reset is required
 func (nullable Nullable[t]) PassError(err error) Nullable[t] {
 	nullable.Error = err
 	return nullable
 }
 
-
-
-
 // check if nullable is empty
 func (nullable Nullable[t]) IsEmpty() bool {
 	return !nullable.filled
 }
-
 
 // handle nullable
 func Handle[t, k any](nullable Nullable[t], def k, handler func(t) k) k {
@@ -88,18 +111,16 @@ func Handle[t, k any](nullable Nullable[t], def k, handler func(t) k) k {
 
 }
 
-
 // get value or default, invokes exception if not pure
 func (nullable Nullable[t]) FromNullable(defaultValue t) t {
 
-	idFunction := func (a t) t {
+	idFunction := func(a t) t {
 		return a
 	}
 
 	return Handle(nullable, defaultValue, idFunction)
 
 }
-
 
 // extra: get value if slice by index
 func SliceIndex[t any](slice []t, index int) Nullable[t] {
@@ -109,7 +130,7 @@ func SliceIndex[t any](slice []t, index int) Nullable[t] {
 	}
 
 	return Nullable[t]{
-		value: slice[index],
+		value:  slice[index],
 		filled: true,
 	}
 
